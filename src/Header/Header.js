0af4884ebf5d824429df1bb40dd431658f710d4f
BMFreed <badmofo14@gmail.com>
2021-06-01 22:56:36 +0300

    First steps

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,15 +1,17 @@
-import './Header.sass';
+import search_icon from './img/search.png'
 
 function Header() {
     return (
         <header className="header">
-        <div className="header__logo">
+        <div className="header__logo header_margin">
           <span className="header__title">OMDb</span>
         </div>
-        <div className="search">
-          <div className="search__flyout_menu"></div>
-          <form className="search__form"></form>
-          <form className="search__action"></form>
+        <div className="search header_margin">
+          <button className="search__flyout_menu button search_padding">Всё</button>
+          <input type="text" placeholder="Искать" className="search__text search_padding"></input>
+          <button className="search__action button search_padding">
+              <img src={search_icon} alt="" />
+          </button>
         </div>
       </header>
     );
